ab7223908422eaf4317fdf7fca83eb354136c4f0
thuyduong <tduong3004@gmail.com>
2024-07-16 15:23:18 +0900

    user store

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,17 +3,41 @@ import { ref } from "vue";
 
 const users = ref([])
 
+// nhung api dang duoc goi
+const usersCache = ref([]);
 // return user
 export const getUserInfo = async(id) => {
     const idf = users.value.findIndex(user => user.id == id)
     if(idf >= 0){
         return users.value[idf]
     }
+
+    const idCachef = usersCache.value.findIndex(userCache => userCache.id == id)
+    if(idCachef >= 0) {
+        return usersCache.value[idCachef].api
+    }
+
     try {
-        return await user_info_get_api(id).then(res => {
-            users.value.push(res)
+        const api =  user_info_get_api(id).then(res => {
+            const idf = users.value.findIndex(user => res.id == user.id)
+            if(idf < 0) {
+                users.value.push(res)
+            }
+            
+            const idCachex = usersCache.value.findIndex(userCache => userCache.id == res.id)
+                if(idCachex >=0){
+                    usersCache.value.splice(idCachex, 1)
+                }
+            console.log("user cache", usersCache.value)
             return res
         })
+
+        usersCache.value.push({
+            id: id,
+            api: api
+        })
+
+        return api
     } catch (error) {
         console.log(error)
     }
